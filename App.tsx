d284a9d1731fbc1985c5a134a352fc19e37f6132
anshikagarg0410 <garganshika0410@gmail.com>
2025-08-30 00:06:24 +0530

    2

--- App.tsx
+++ App.tsx
@@ -8,29 +8,29 @@ import LoginPage from './pages/LoginPage';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import ProtectedRoute from './components/ProtectedRoute';
-import { AuthProvider } from './contexts/AuthContext';
+// We do NOT import AuthProvider here anymore
 
 const App: React.FC = () => {
   return (
-    <AuthProvider>
-      <HashRouter>
-        <div className="flex flex-col min-h-screen text-gray-700">
-          <Navbar />
-          <main className="flex-grow container mx-auto px-4 py-8">
-            <Routes>
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/" element={<HomePage />} />
-              <Route element={<ProtectedRoute />}>
-                <Route path="/gallery" element={<GalleryPage />} />
-                <Route path="/notes" element={<NotesPage />} />
-                <Route path="/proposal" element={<ProposalPage />} />
-              </Route>
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </HashRouter>
-    </AuthProvider>
+    // The extra <AuthProvider> wrapper has been removed from this file.
+    // The correct one is in index.tsx.
+    <HashRouter>
+      <div className="flex flex-col min-h-screen text-gray-700">
+        <Navbar />
+        <main className="flex-grow container mx-auto px-4 py-8">
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/" element={<HomePage />} />
+            <Route element={<ProtectedRoute />}>
+              <Route path="/gallery" element={<GalleryPage />} />
+              <Route path="/notes" element={<NotesPage />} />
+              <Route path="/proposal" element={<ProposalPage />} />
+            </Route>
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </HashRouter>
   );
 };
 
